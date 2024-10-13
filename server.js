0e24ece7bf04efac17021b51f0691417a033110e
tamo <tamo@example.com>
2024-10-13 07:34:03 -0700

    added new end points apis /users /airports

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import path from 'path'; // Import the path module to work with file paths
 import fs from 'fs';     // Import the fs module to read files
 import {fileURLToPath} from 'url';
-import {PARAMS, PORT} from "./src/constants.js";
+import {PARAMS, PORT, SERVER_STATUS} from "./src/constants.js";
 
 const app = express();
 const port = PORT;
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 
 // Endpoint for '/entry' route
-app.get('/entry', (request, response) => {
+app.get('/mocks', (request, response) => {
 
     let fileName;
     const queryKey = Object.keys(request.query)[0];
@@ -29,7 +29,7 @@ app.get('/entry', (request, response) => {
 
     // If no valid query parameter is provided, return an error
     if (!fileName) {
-        return response.status(400).json({error: 'Invalid query parameter. Use proper param key'});
+        return response.status(SERVER_STATUS.INVALID_PARAM).json({error: 'Invalid query parameter. Use proper param key'});
     }
 
     // Build the full path to the JSON file
@@ -38,13 +38,85 @@ app.get('/entry', (request, response) => {
     // Check if the file exists
     fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
-            return response.status(404).json({error: `File ${fileName} not found.`});
+            return response.status(SERVER_STATUS.NOT_FOUND).json({error: `File ${fileName} not found.`});
         }
 
         // Read the JSON file and send it as a response
         fs.readFile(filePath, 'utf8', (err, data) => {
             if (err) {
-                return response.status(500).json({error: 'Error reading the file.'});
+                return response.status(SERVER_STATUS.INTERNAL_SERVER_ERROR).json({error: 'Error reading the file.'});
+            }
+
+            // Parse the JSON and send it as the response
+            response.json(JSON.parse(data));
+        });
+    });
+});
+
+const createFileWithExtensions = (file, extension) => {
+    const newName = file.replace(/\//g, "");  // Removes all "/"
+    const fileName = (file && newName) ? `${newName}.${extension}` : "";
+    return fileName;
+}
+
+app.get('/users', (request, response) => {
+
+
+    const fileName = createFileWithExtensions(request.url, "json");  // Output: "file name"
+
+    // If no valid query parameter is provided, return an error
+    if (!fileName) {
+        return response.status(SERVER_STATUS.INVALID_PARAM).json({error: 'Invalid query parameter. Use proper param key'});
+    }
+
+    const queryKey = fileName.split(".json")[0];
+
+    // Build the full path to the JSON file
+    const filePath = path.join(__dirname, `src/resources/${queryKey}`, fileName);
+
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            return response.status(SERVER_STATUS.NOT_FOUND).json({error: `File ${fileName} not found.`});
+        }
+
+        // Read the JSON file and send it as a response
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                return response.status(SERVER_STATUS.INTERNAL_SERVER_ERROR).json({error: 'Error reading the file.'});
+            }
+
+            // Parse the JSON and send it as the response
+            response.json(JSON.parse(data));
+        });
+    });
+});
+
+app.get('/airports', (request, response) => {
+
+
+    const fileName = createFileWithExtensions(request.url, "json");  // Output: "file name"
+
+    // If no valid query parameter is provided, return an error
+    if (!fileName) {
+        return response.status(SERVER_STATUS.INVALID_PARAM).json({error: 'Invalid query parameter. Use proper param key'});
+    }
+
+    const queryKey = fileName.split(".json")[0];
+
+    // Build the full path to the JSON file
+    const filePath = path.join(__dirname, `src/resources/${queryKey}`, fileName);
+
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            return response.status(SERVER_STATUS.NOT_FOUND).json({error: `File ${fileName} not found.`});
+        }
+
+        // Read the JSON file and send it as a response
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                return response.status(SERVER_STATUS.INTERNAL_SERVER_ERROR).json({error: 'Error reading the file.'});
             }
 
             // Parse the JSON and send it as the response
@@ -55,5 +127,5 @@ app.get('/entry', (request, response) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at /entry`);
+    console.log(`Server running at https://jsonjet.vercel.app`);
 });
